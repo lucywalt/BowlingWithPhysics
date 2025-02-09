4127dabdfb3efebef245318f3fa9194495a3efb0
lucywalt <lucywltrs@gmail.com>
2025-02-09 15:36:37 -0800

    Created main objects and scripts

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+    
+    void Start()
+    {
+        //Grabbing a reference to RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        //Add a listenr to the OnSpacePressed event
+        // When the key is pressed, the LaunchBall method will be called
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
